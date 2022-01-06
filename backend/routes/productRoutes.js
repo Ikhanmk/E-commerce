426adb974d20fbe22904ccec79bf661f60870d1a
ikhanmk <ikhanmk@gmail.com>
2022-01-07 02:58:33 +0530

    User routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,12 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/products/new").post(isAuthenticatedUser,authorisedRoles("admin"), createProduct);
+router.route("/admin/products/new").post(isAuthenticatedUser,authorisedRoles("admin"), createProduct);
 
-router.route("/products/:id").put(isAuthenticatedUser, authorisedRoles("admin"), updateProducts).delete(isAuthenticatedUser,authorisedRoles("admin"), deleteProducts).get(getProductDetails)
+router.route("/admin/products/:id").put(isAuthenticatedUser, authorisedRoles("admin"), updateProducts).delete(isAuthenticatedUser,authorisedRoles("admin"), deleteProducts)
 
 
+router.route("/product/:id").get(getProductDetails);
 
 
 module.exports = router
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword } = require("../controllers/userController");
+const {isAuthenticatedUser, authorisedRoles} = require("../middleware/auth")
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUser,getSingleUser, updateUserRole, deleteUser } = require("../controllers/userController");
+const { createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -7,6 +9,16 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logoutUser);
+router.route("/me").get(isAuthenticatedUser ,getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+router.route("/admin/users").get(isAuthenticatedUser, authorisedRoles("admin"), getAllUser);
+router.route("/admin/user/:id")
+.get(isAuthenticatedUser, authorisedRoles("admin"), getSingleUser)
+.put(isAuthenticatedUser, authorisedRoles("admin"), updateUserRole)
+.delete(isAuthenticatedUser, authorisedRoles("admin"), deleteUser)
+router.route("/review").put(isAuthenticatedUser, createProductReview)
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser, deleteReview)
 
 
 module.exports = router;
